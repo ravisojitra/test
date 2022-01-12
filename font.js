3869b7f26ed84854b91850d2819a2650b9c6296d
Ravi Sojitra <ravisojitra79@gmail.com>
2022-01-12 17:36:20 +0530

    Delete font.js

--- font.js
+++
@@ -1,6 +0,0 @@
-export default `
- @font-face { src: url("./passiononeregular400.ttf"); font-family : "Passion One"; font-style : Regular; font-weight : 400;  }
- @font-face { src: url("./poppinssemibold600.ttf"); font-family : "Poppins"; font-style : SemiBold; font-weight : 600;  }
- @font-face { src: url("./poppinsmedium500.ttf"); font-family : "Poppins"; font-style : Medium; font-weight : 500;  }
- @font-face { src: url("./poppinsregular400.ttf"); font-family : "Poppins"; font-style : Regular; font-weight : 400;  }
- @font-face { src: url("./righteousregular400.ttf"); font-family : "Righteous"; font-style : Regular; font-weight : 400;  }`

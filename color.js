f8a924a950e5b54e424950f4d71993152885add1
Ravi Sojitra <ravisojitra79@gmail.com>
2022-01-12 17:34:58 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,14 @@
+export default `:root { 
+--amber_900:#fc7000; 
+--bluegray_800:#36384d; 
+--gray_400:#bababa; 
+--black_900_1a:#0000001a; 
+--purple_800_67:#73299667; 
+--gray_900:#0f0f2e; 
+--purple_800_99:#73299699; 
+--purple_801:#6e2191; 
+--purple_800:#631487; 
+--white_A700:#ffffff; 
+--purple_50:#f5e3ff; 
+--gray_100:#f0f2f7; 
+}`
